be5cf4692eb4b63ef03a296b4b5ba4d0a56a10d3
mutiara126 <salsamutiara1812@gmail.com>
2024-11-21 16:39:28 +0700

    untuk membuat object jenis kelamin

---
+++ Prak7/app/src/main/java/com/example/prak7/model/JenisKelamin.kt
@@ -0,0 +1,12 @@
+package com.example.prak7.model
+
+import com.example.prak7.R
+
+// Untuk membuat dummy data (data sementara)
+
+object JenisKelamin {
+    val listJK = listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
