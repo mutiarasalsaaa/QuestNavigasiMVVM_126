66aca27c54a1c2405035ac7ef0e9e67f3e09c274
mutiara126 <salsamutiara1812@gmail.com>
2024-11-21 16:31:24 +0700

    menyimpan informasi siswa

---
+++ Prak7/app/src/main/java/com/example/prak7/model/DataSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.prak7.model
+
+data class DataSiswa(
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val NIM: String = "",
+    val email: String = "",
+    val notelp: String = "",
+)
