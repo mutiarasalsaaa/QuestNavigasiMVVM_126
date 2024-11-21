613d30dbe83f0692445ec4a218f47404a68727b5
mutiara126 <salsamutiara1812@gmail.com>
2024-11-21 16:18:27 +0700

    menggunakan variabel yg dikelola oleh compose (mutableStateOf) utk melacak nilai setiap field input

---
+++ Prak7/app/src/main/java/com/example/prak7/ui/view/FormView.kt
@@ -0,0 +1,41 @@
+package com.example.prak7.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+) {
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var notlp by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(nama, nim, email, notlp, gender, alamat)
+
